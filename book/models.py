808760a4e5b504533f43f7013acfbc26e659d334
lukaFitstic <barbieri.luka@fitstic-edu.com>
2024-04-20 16:16:16 +0200

    progetto base, funziona

---
+++ book/models.py
@@ -0,0 +1,21 @@
+from django.db import models
+
+# Create your models here.
+class Autore(models.Model):
+    nome = models.CharField(max_length=100)
+    cognome = models.CharField(max_length=100)
+    eta = models.IntegerField()
+    def __str__(self):
+        return self.nome
+
+class Book(models.Model):
+    title = models.CharField(max_length=100)
+    author = models.ForeignKey(Autore,
+                               on_delete=models.CASCADE,
+                               default=None,
+                               null=True)
+    year = models.IntegerField()
+    pages = models.IntegerField()
+    body = models.TextField()
+    def __str__(self):
+        return self.title
