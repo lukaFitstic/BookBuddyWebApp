3d9bbfb5ca99f6df1597aff7191ea7a1c5a2bf7e
Nash <gabriele.nascetti12@gmail.com>
2024-04-20 18:51:13 +0200

    Aggiunte le pagine dei singoli generi

--- book/urls.py
+++ book/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import BookListView, AzioneListView
+from .views import *
 urlpatterns = [
     path('', BookListView.as_view(), name='home'),
     path('azione/', AzioneListView.as_view(), name='azione'),
+    path('giallo/', GialloListView.as_view(), name='giallo')
 ]
