3e42a83a14da7cf44f592babc849d3642ddf41c4
Simeone Zuffi <zuffi.simeone@fitstic-edu.com>
2024-04-21 15:47:05 +0200

    Implementazione login e signup

---
+++ LoginSignUp/views.py
@@ -0,0 +1,34 @@
+from django.shortcuts import render, redirect
+from .forms import RegisterForm
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+# Create your views here.
+def signup(request):
+    if request.method == 'POST':
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return render(request, 'login.html', {'success': "Registration successful. Please login."})
+        else:
+            error_message = form.errors.as_text()
+            return render(request, 'signup.html', {'error': error_message})
+
+    return render(request, 'signup.html')
+
+def login(request):
+    if request.method=="POST":
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+        user = authenticate(request, username=email, password=password)
+        if user is not None:
+            #login(request, user)
+            return redirect("/")
+        else:
+            return render(request, 'login.html', {'error': "Invalid credentials. Please try again."})
+
+    return render(request, 'login.html')
+
+@login_required
+def logout(request):
+    return render(request, 'login.html')
+
